e9ce2b09c37ad5468d02bec455b501a67e85ca61
matthew butler <virtualprodigyllc@gmail.com>
2017-11-29 16:24:35 -0500

    working on creating an npm package https://medium.com/@OCombe/how-to-publish-a-library-for-angular-2-on-npm-5f48cdabf435

---
+++ vp-alpha-scroll.ts
@@ -0,0 +1,17 @@
+
+
+//enums
+export {CasingEnums} from "./src/assets/enums/casing.enums";
+// models
+export {AlphaScrollInit} from "./src/assets/models/AlphaScrollInit";
+export {AlphaScrollItem} from "./src/assets/models/AlphaScrollItem";
+export {AlphaScrollGroup} from "./src/assets/models/AlphaScrollGroup";
+export {AlphaScrollGroups} from "./src/assets/models/AlphaScrollGroups";
+
+//modules
+export {AlphaScrollModule} from "./src/components/alpha-scroll.module"
+
+//components
+export {AlphaScrollComponent} from "./src/components/alpha-scroll/alpha-scroll"
+// providers
+export {AlphaDataProvider} from "./src/providers/alpha-data/alpha-data";
